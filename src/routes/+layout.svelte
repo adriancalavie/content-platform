4815a6c3855f417e0eb45779d65fb0f97aefc41a
Adrian Călăvie <adrian.calavie06@gmail.com>
2023-11-13 16:56:19 +0200

    Add Flowbite UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import '../app.postcss';
+	import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+	import { TerminalSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<TerminalSolid class="mr-2" size="lg" alt="Content Platform Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Content Platform
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/login">Log in</NavLi>
+		<NavLi href="/about">About</NavLi>
+	</NavUl>
+</Navbar>
+<div class="p-8">
+	<slot />
+</div>
